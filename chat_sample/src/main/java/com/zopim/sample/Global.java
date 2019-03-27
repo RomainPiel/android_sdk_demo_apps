162c7f165164e78c903b5a4f747a8b006d4441fb
Romain Piel <romain.piel@deliveroo.co.uk>
2019-03-27 16:05:30 +0100

    Disable Firebase notifications

--- chat_sample/src/main/java/com/zopim/sample/Global.java
+++ chat_sample/src/main/java/com/zopim/sample/Global.java
@@ -2,12 +2,7 @@ package com.zopim.sample;
 
 import android.app.Application;
 import android.util.Log;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.zendesk.logger.Logger;
-import com.zendesk.util.StringUtils;
 import com.zopim.android.sdk.api.ZopimChat;
 import com.zopim.android.sdk.model.VisitorInfo;
 
@@ -21,11 +16,6 @@ public class Global extends Application {
      * Account key can be found in Zopim Dashboard at the <a href="https://dashboard.zopim.com/#widget/getting_started">Getting Started Page</a>
      */
     private final static String ACCOUNT_KEY = ""; // NB: Replace this key with your Zopim account key
-    private final static String PROJECT_ID = ""; // Firebase console -> Project settings -> General -> Project Id
-    private final static String API_KEY = ""; // Firebase console -> Project settings -> Cloud messaging -> Server Key
-    private final static String FCM_SENDER_ID = ""; // Firebase console -> Project settings -> Cloud messaging -> Sender ID
-
-    private static boolean missingCredentials = false;
 
     @Override
     public void onCreate() {
@@ -33,11 +23,6 @@ public class Global extends Application {
 
         // Enable logging
         Logger.setLoggable(true);
-
-        if (StringUtils.isEmpty(ACCOUNT_KEY)) {
-            missingCredentials = true;
-            return;
-        }
 
         // Sample breadcrumb
         ZopimChat.trackEvent("Application created");
@@ -79,41 +64,9 @@ public class Global extends Application {
 //            ZopimChat.setVisitorInfo(visitorData);
         }
 
-        // Initialise Firebase app to receive push notifications
-        initFirebase();
-
         // clear visitor info. Visitor info storage can be disabled at chat initialization
         VisitorInfo emptyVisitorInfo = new VisitorInfo.Builder().build();
         ZopimChat.setVisitorInfo(emptyVisitorInfo);
         Log.v("Zopim Chat Sample", "Visitor info erased.");
-    }
-
-    private void initFirebase() {
-        if (PROJECT_ID.isEmpty() || API_KEY.isEmpty() || FCM_SENDER_ID.isEmpty()) {
-            missingCredentials = true;
-            return;
-        }
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setApplicationId(PROJECT_ID)
-                .setApiKey(API_KEY)
-                .setGcmSenderId(FCM_SENDER_ID)
-                .build();
-
-        FirebaseApp.initializeApp(this, options);
-
-        try {
-            String token = FirebaseInstanceId.getInstance().getToken();
-            if (token != null) {
-                Log.d(LOG_TAG, "Obtained FCM token");
-                ZopimChat.setPushToken(token);
-            }
-        } catch (IllegalStateException e) {
-            Log.d(LOG_TAG, "Error requesting FCM token");
-        }
-    }
-
-    static boolean isMissingCredentials() {
-        return missingCredentials;
     }
 }
